e0b88e7df430164be2bb81f7bec46d8319c4b9d5
Alejandro Costa <costa.claudio.alejandro@gmail.com>
2023-05-06 11:14:18 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { dolar_blue_venta } from "./components/NewApi";
 import Table from "./components/Table/Table";
 import Hero from "./components/Hero";
 
 const Home = async () => {
-  const DOVenta = await dolar_blue_venta();
-
   return (
     <>
       <Hero />
