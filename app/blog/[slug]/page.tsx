0a38feb7cb240724f6d3531a49192b4089eb93fd
Alejandro Costa <costa.claudio.alejandro@gmail.com>
2023-05-06 17:13:53 -0300

    first commit

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -40,7 +40,7 @@ async function Post({ params }: { params: { slug: string } }) {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="sm:text-center lg:text-left">
             <h1 className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
-              <span className="block xl:inline text-center">titulo</span>{" "}
+              <span className="block xl:inline text-center">{post?.title}</span>{" "}
             </h1>
           </div>
         </div>
