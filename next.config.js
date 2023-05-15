66a756fe14be284ae45659e01d0d42aa89f93eea
Alejandro Costa <costa.claudio.alejandro@gmail.com>
2023-05-15 17:38:12 -0300

    project updated 2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  /* config options here */
-};
+module.exports = {
+  env: {
+    API_URL: "http://127.0.0.1:3000",
+  },
 
-module.exports = nextConfig;
+  basePath: "/my-app",
+};
