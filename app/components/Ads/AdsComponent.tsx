97e5a6d7932000d11a39636a5c363713dc520779
Alejandro Costa <costa.claudio.alejandro@gmail.com>
2023-05-16 20:30:53 -0300

    ads placeholder left

--- app/components/Ads/AdsComponent.tsx
+++ app/components/Ads/AdsComponent.tsx
@@ -1,7 +1,10 @@
 function AdsComponent() {
   return (
     <>
-      <div className=" h-16 my-2 md:h-full md:my-0"></div>
+      <div className=" h-16 my-2 md:h-full md:my-0">
+        {/* Ezoic - Left/top Ad - sidebar_middle */}
+        <div id="ezoic-pub-ad-placeholder-110"> </div>
+      </div>
     </>
   );
 }
