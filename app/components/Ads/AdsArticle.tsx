1e0ac99c6a5661a96c9d6fb3d9b1ddaf9e02e64a
Alejandro Costa <costa.claudio.alejandro@gmail.com>
2023-05-16 22:17:11 -0300

    ads placeholder

--- app/components/Ads/AdsArticle.tsx
+++ app/components/Ads/AdsArticle.tsx
@@ -1,7 +1,9 @@
 function AdsArticle() {
   return (
     <>
-      <div className=" h-16 w-full"> </div>
+      <div className=" h-16 w-full">
+        <div id="ezoic-pub-ad-placeholder-112"> </div>
+      </div>
     </>
   );
 }
--- app/components/Ads/AdsComponentHorizontal.tsx
+++ app/components/Ads/AdsComponentHorizontal.tsx
@@ -1,7 +1,9 @@
 function AdsComponentHorizontal() {
   return (
     <>
-      <div className=" hidden md:h-16 md:block "></div>
+      <div className=" hidden md:h-16 md:block ">
+        <div id="ezoic-pub-ad-placeholder-113"> </div>
+      </div>
     </>
   );
 }
