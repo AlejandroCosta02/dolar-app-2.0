c51fc218c0533698302211e4a81c8ff55885598d
Alejandro Costa <costa.claudio.alejandro@gmail.com>
2023-05-20 11:55:13 -0300

    ads

--- app/dolarblue/compra/page.tsx
+++ app/dolarblue/compra/page.tsx
@@ -37,11 +37,13 @@ async function DB_Compra() {
 
           <Conversion_2 dolarblue={parseFloat(dolarblue_compra)} />
         </div>
-        <div className="col-span-12 md:col-span-2"></div>
+        <div className="col-span-12 md:col-span-2">
+          <AdsDisplay />
+        </div>
       </div>
-      <div className="flex flex-col mx-auto items-center py-3">
+      {/* <div className="flex flex-col mx-auto items-center py-3">
         <AdsDisplay />
-      </div>
+      </div> */}
     </>
   );
 }
